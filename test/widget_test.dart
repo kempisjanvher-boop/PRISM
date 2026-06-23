211937a4fa35fc464ca6de1c2905e129eea0654a
kirt <kirtrommarn@gmail.com>
2026-06-23 19:55:02 +0800

    pages

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:prism_system/main.dart';
 void main() {
   testWidgets('App smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const PrismApp());
+    await tester.pumpWidget(PrismApp());
 
     // Verify that the app builds without errors
     expect(find.byType(MaterialApp), findsOneWidget);
